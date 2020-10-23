a371dd87ec67a95a54903dfb04d3ab438e062fda
q451234 <1598576037@qq.com>
2020-10-23 13:06:35 +0800

    pc

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ var mysql  = require('mysql');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'Admin',
-  password : '741025',
+  user     : 'root',
+  password : '123456',
   port: '3306',
   database: 'userinfo'
 });
